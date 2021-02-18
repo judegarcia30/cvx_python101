d43f81e26c2aa5f0b83cfd0a6d0ae479144aa8da
Russel Jude Garcia <rugm@chevron.com>
2021-02-18 11:08:23 +0800

    Added additional exercises

---
+++ exercises/function_exercise.py
@@ -0,0 +1,28 @@
+# Write a function func1() such that it can accept a variable length of  argument and print all arguments value
+# Expected Output:
+# After func1(router, PHRCRG1):
+# router
+# PHRCRG1
+# func1(10, 20, 30):
+# 10
+# 20
+# 30
+
+# Write a function calculation() such that it can accept two variables and calculate the addition and subtraction of it. And also it must return both addition and subtraction in a single return call
+# For example:
+
+def calculation(a, b):
+    pass
+    # Your Code
+result = calculation(40, 10)
+print(result)
+# Expected Output: (50, 30)
+
+# Create a function showDevice() in such a way that it should accept device name, and it’s type and display both, and if the type is missing in function call it should show it as router
+# Expected Output:
+# showDevice("PHRCRG1", "router")
+# showDevice("PHRCRG1")
+# Should Produce:
+# Device PHRCRG1 is a router
+# Device PHRCRG1 is a router
+
---
+++ exercises/loops_if_else.py
@@ -0,0 +1,50 @@
+# Print First 10 natural numbers using while loop
+# Expected output:
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# Print multiplication table of given number
+# For example num = 2 so the output should be
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# 20
+
+# Given a list iterate it and display numbers which are divisible by 5 and if you find number greater than 150 stop the loop iteration
+list1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+# Expected output
+# 15
+# 55
+# 75
+# 150
+# Reverse the following list using for loop
+list1 = [10, 20, 30, 40, 50]
+# Expected output:
+# 50
+# 40
+# 30
+# 20
+# 10
+
+# Write a loop to find the factorial of any number
+# The factorial (symbol: !) means to multiply all whole numbers from our chosen number down to 1.
+# For example: calculate the factorial of 5
+num = 5
+# 5! = 5 × 4 × 3 × 2 × 1 = 120
+# Expected output:
+# 120
